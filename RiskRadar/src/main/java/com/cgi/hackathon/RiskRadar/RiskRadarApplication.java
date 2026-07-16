d4d748f503a9d4def1bf476d97d88c7d27f4a26e
harijendra <informharijendra@gmail.com>
2026-07-16 19:05:59 +0530

    Add files via upload

---
+++ RiskRadar/src/main/java/com/cgi/hackathon/RiskRadar/RiskRadarApplication.java
@@ -0,0 +1,13 @@
+package com.cgi.hackathon.RiskRadar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskRadarApplication.class, args);
+	}
+
+}
---
+++ RiskRadar/src/test/java/com/cgi/hackathon/RiskRadar/RiskRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgi.hackathon.RiskRadar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
